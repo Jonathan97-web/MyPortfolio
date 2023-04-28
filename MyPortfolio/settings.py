407d1714f0bce5093bed86d3f74a334a90bcbf5a
Jonathan97-web <zakrisson97@gmail.com>
2023-04-28 19:42:42 +0000

    Fix ALLOWED_HOST error in settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["myportfoliopp4.herokuapp.com," "localhost, *.gitpod.io"]
+ALLOWED_HOSTS = ["myportfoliopp4.herokuapp.com", "localhost", "https://*.gitpod.io"]
 
 
 # Application definition
