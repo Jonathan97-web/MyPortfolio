eababdb4f43451d7ca7a2dd5dc273580cd54e0c6
Jonathan97-web <zakrisson97@gmail.com>
2023-05-04 12:24:19 +0000

    Remove summernote urls

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
     path('', include('projects.urls'), name='myportfolio_urls'),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profiles.urls'), name='profiles_url'),
