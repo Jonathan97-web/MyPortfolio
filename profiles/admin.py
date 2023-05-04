860f3499dc3573583797f971f9f235a966c7a857
Jonathan97-web <zakrisson97@gmail.com>
2023-05-04 12:07:43 +0000

    Add profiles admin file

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class profileAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name',)
+    search_fields = ['first_name', 'last_name',]
