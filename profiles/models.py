4ccc9a0e3d4cfebd2c744537ce391e6a1e236f4f
Jonathan97-web <zakrisson97@gmail.com>
2023-05-04 11:12:56 +0000

    Add initial profiles app

---
+++ profiles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    intro = models.CharField(max_length=100, null=True, blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    linkedin = models.URLField(null=True, blank=True)
+    github = models.URLField(null=True, blank=True)
+    portfolio = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
