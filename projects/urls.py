b570254456a4690ccaaddf5c3119f71740404691
Jonathan97-web <zakrisson97@gmail.com>
2023-05-06 14:45:10 +0000

    Fix urlpattern in urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ProjectList.as_view(), name="home"),
-    path('<int:id>/<slug:slug>/', views.ProjectDetail.as_view(), name='project_detail'),
+    path('<int:id>/<slug:slug>/', views.ProjectDetail, name='project_detail')
 ]
