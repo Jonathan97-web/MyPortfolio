52b305ab596e15e8d0a4955c15e5982557c0bbdb
Jonathan97-web <zakrisson97@gmail.com>
2023-05-07 00:12:49 +0000

    Fix onetoonefield for uniqueness in models.py

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     intro = models.CharField(max_length=100, null=True, blank=True)
     image = CloudinaryField('image', default='placeholder')
     linkedin = models.URLField(null=True, blank=True)
