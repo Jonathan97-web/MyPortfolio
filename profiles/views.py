a6033b5083ed1f66479753fb6e6300298e7671fb
Jonathan97-web <zakrisson97@gmail.com>
2023-05-08 22:22:16 +0000

    Add view_profile and reformatted profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,44 +1,32 @@
-from django.shortcuts import render, redirect
-from django.views import generic, View
-from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.urls import reverse
 from .models import Profile
+from .forms import UpdateProfileForm, UpdateUserForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 
-def profile(request):
-    return render(request, 'profile.html')
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'register.html', {'form': form})
-
-# Update it here
+# Profile for the logged in user
 @login_required
-def profile(request):
+def profile(request, id):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+        p_form = UpdateProfileForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile') # Redirect back to profile page
-
+            return redirect(to='/profile/1')
+            # next = request.POST.get('next', '/profile/1')
+            # return HttpResponseRedirect(next)
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        u_form = UpdateUserForm(instance=request.user)
+        p_form = UpdateProfileForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
@@ -46,3 +34,10 @@ def profile(request):
     }
 
     return render(request, 'profile.html', context)
+
+# View profile
+
+
+def view_profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request, 'profile_detail.html', {"user": user})
