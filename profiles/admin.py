2338386788a7dfd54f2724b709e918c7d0a45223
Jonathan97-web <zakrisson97@gmail.com>
2023-05-08 22:12:42 +0000

    Add id list and search field

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,5 +5,5 @@ from .models import Profile
 @admin.register(Profile)
 class profileAdmin(admin.ModelAdmin):
 
-    list_display = ('first_name', 'last_name',)
-    search_fields = ['first_name', 'last_name',]
+    list_display = ('first_name', 'last_name', 'id')
+    search_fields = ['first_name', 'last_name', 'id']
