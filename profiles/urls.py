88722d15336832acffd8ce0023bca030a84db532
Jonathan97-web <zakrisson97@gmail.com>
2023-05-08 22:14:00 +0000

    Add profile paths for unique profile viewing

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-    path("", views.profile, name="profile"),
+    path('profile/<int:id>', views.profile, name="profile"),
+    path('profile/<str:username>', views.view_profile, name='profile_detail')
 ]
