14b819f4cc67084912a56fecca17e7a24cfd06bb
Jonathan97-web <zakrisson97@gmail.com>
2023-05-09 15:54:30 +0000

    Add paths to project and comments edit and delete

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 urlpatterns = [
     path("", views.ProjectList.as_view(), name="home"),
     path('<int:id>/<slug:slug>/', views.ProjectDetail, name='project_detail'),
-    path('project_create/', views.CreateProject, name='project_create')
+    path('project_create/', views.CreateProject, name='project_create'),
+    path('project_edit/<int:id>', views.edit_project, name='project_edit'),
+    path('project_delete/<int:id>', views.delete_project, name='project_delete'),
+    path('comment_edit/<int:id>', views.edit_comment, name='comment_edit'),
+    path('comment_delete/<int:id>', views.delete_comment, name='comment_delete'),
+
 ]
