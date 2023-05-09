c172f206cdcd32ce9e184fd7ed075cd84008e5c3
Jonathan97-web <zakrisson97@gmail.com>
2023-05-09 03:21:50 +0000

    Add createprojectform for creating a post

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
-from .models import Comment
+from .models import Comment, Project
+from profiles.models import Profile
 from django import forms
 
 
@@ -6,3 +7,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('email', 'body')
+
+
+class CreateProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ('title', 'content', 'image')
