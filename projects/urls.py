6147291ec051f35ea00de6513c65b51ed5c99ddb
Jonathan97-web <zakrisson97@gmail.com>
2023-05-09 03:22:48 +0000

    Add path to create project urls

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ProjectList.as_view(), name="home"),
-    path('<int:id>/<slug:slug>/', views.ProjectDetail, name='project_detail')
+    path('<int:id>/<slug:slug>/', views.ProjectDetail, name='project_detail'),
+    path('project_create/', views.CreateProject, name='project_create')
 ]
